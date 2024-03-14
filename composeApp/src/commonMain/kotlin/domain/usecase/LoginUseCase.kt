0d0853fb9bafa2eade49c80da0eda06144659379
Jahid Hasan Naiem <jahid.hasan@monstar-lab.com>
2024-03-14 13:36:46 +0600

    feat: Add login

--- composeApp/src/commonMain/kotlin/domain/usecase/LoginUseCase.kt
+++ composeApp/src/commonMain/kotlin/domain/usecase/LoginUseCase.kt
@@ -2,20 +2,12 @@ package domain.usecase
 
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
 import domain.repository.AuthRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 
 class LoginUseCase(
     private val authRepository: AuthRepository,
 ) {
     @NativeCoroutines
-    suspend operator fun invoke(email: String, password: String) = flow {
-        emit(
-            runCatching {
-                authRepository.authenticate(email, password)
-            }
-        )
+    suspend operator fun invoke(email: String, password: String) = runCatching {
+        authRepository.authenticate(email, password)
     }
-        .flowOn(Dispatchers.Default)
 }
